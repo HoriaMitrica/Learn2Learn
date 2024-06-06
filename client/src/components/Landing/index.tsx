8c8f03cc786083d2cc41dbd6df621c622c06990a
Horia Mitrica <113677938+HoriaMitrica@users.noreply.github.com>
2024-06-06 09:58:58 +0300

    added navbar and style (#2)

---
+++ client/src/components/Landing/index.tsx
@@ -0,0 +1,16 @@
+import "./style.scss"
+
+export const Landing = () => {
+  return (
+    <>
+      <section id="landing">
+        <h1>
+          Learn to Learn
+        </h1>
+        <div>
+          Înscrierile încep de pe 10 iunie!
+        </div>
+      </section>
+    </>
+  )
+}
--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -1,15 +1,33 @@
+
+import { Link } from "react-scroll"
+import { AssetPaths } from "../../models/enum"
 import "./style.scss"
 
+export const Navbar: React.FC = () => {
 
-export const Navbar = () => {
     return (
         <>
-            <div className="testClassOne">
-                gilroy
-            </div>
-            <div className="anotherTestClass">
-                BOldstrom LEARN TO LEARN
-            </div>
+        {/* <img src={AssetPaths.Logo} alt="Logo" className="logo-img" /> */}
+            <header id='nav-wrapper'>
+                <nav >
+                <div className='nav right'>
+                <Link className='nav-link' to='landing' spy={true} smooth={true} offset={0} duration={100}>
+                        <span className='nav-link-span'>
+                            <span className='u-nav'>
+                                Home
+                            </span>    
+                        </span> 
+                    </Link>
+                    <Link className='nav-link' to='about' spy={true} smooth={true} offset={0} duration={100}>
+                        <span className='nav-link-span'>
+                            <span className='u-nav'>
+                                About
+                            </span>    
+                        </span> 
+                    </Link>
+                </div>
+                </nav>
+            </header >
         </>
     )
 }
