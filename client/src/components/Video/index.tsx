ffa85dc0204f3a9f9b7a60dd9f1be095d8ddefcc
HoriaMitrica <horia.mitrica@gmail.com>
2024-06-07 12:47:02 +0300

    to be worked on mediaAparitions

--- client/src/components/Video/index.tsx
+++ client/src/components/Video/index.tsx
@@ -3,15 +3,11 @@ import { VideoProps } from "../../models/media_aparitions"
 export const Video: React.FC<VideoProps> = ({ ...props }) => {
     return (
         <>
-            <div className="video">
-                <div >
-                    <iframe style={{border:"none"}} width="400px" height="250px" src={props.link} title="YouTube video " allowFullScreen></iframe>
-                </div>
-                <div>
-                    {props. title}
-                </div>
-                <div>
-                    {props.description}
+            <div>
+                <iframe src={props.link} title="YouTube video" allowFullScreen></iframe>
+                <div className="text-container">
+                    <div>{props.title}</div>
+                    <div>{props.description}</div>  
                 </div>
             </div>
         </>
